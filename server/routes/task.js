fffaad3bf6390278ec9b84b5418704f2c834bfd7
Michael <msaputra93@yahoo.com>
2019-04-05 10:45:20 +0700

    fix conflict model task

--- server/routes/task.js
+++ server/routes/task.js
@@ -4,6 +4,6 @@ const TaskController = require('../controllers/taskController')
 taskRouter.post('/', TaskController.create)
 taskRouter.get('/', TaskController.findAll)
 taskRouter.get('/search', TaskController.filterByName)
-taskRouter.patch('/:id', Task.Controller.patchStatus)
+taskRouter.patch('/:id', TaskController.patchStatus)
 
 module.exports = taskRouter
