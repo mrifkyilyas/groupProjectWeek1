7474994fae8982c4f3576ead812cbfa49ca632a9
qoyyima22 <qoyyimafiassalam@gmail.com>
2019-04-05 11:51:14 +0700

    tambah Client Baru

--- client/js/index2.js
+++ client/js/index2.js
@@ -1,17 +1,34 @@
 var x
 var y
-$('document').ready(function(){
+$('document').ready(function () {
     $('.form').hide()
 })
 
-$('td.date').click(function(){
+$('td.date').click(function () {
     x = event.target.id
     console.log(event.target.id)
     $('.form').show()
+    let date = ($(`td#${x}`).text() + " " + $(`#monthYear`).text())
+    let arr = date.split(' ')
+    switch (arr[1]) {
+        case 'April':
+            arr[1] = '04'
+    }
+
+    let isoDate = arr[2] + "-" + arr[1] + "-" + arr[0]
+    isoDate = new Date(isoDate).toISOString()
+    $('input#date').text(isoDate)
+    console.log(isoDate)
 })
 
-$('#form').submit(function(){
+$('#form').submit(function () {
     $('.form').hide()
+    // let inputObjek = {
+    //     name: $('input#ToDo'),
+    //     start: isoDate,
+    //     description: ,
+    //     location: ,
+    // }
 })
 
 // $('input').click(function(){
